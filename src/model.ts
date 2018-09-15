8d8323109faad65db2c448735a0b9170594e1099
Grzegorz Abramczyk <abramczykg@gmail.com>
2018-09-15 21:04:05 +0200

    Progress item model

---
+++ src/model.ts
@@ -0,0 +1,13 @@
+export interface Category {
+  name: string
+  parent?: Category
+}
+
+export interface ProgressItem {
+  summary: string // Textual description of the item
+  defined?: Date // date time of item definition
+  resolved?: Date // date time of item resolution
+  labels?: string[] // set of additional classification information
+  status: 'defined' | 'resolved' // item status
+  categories?: Category[] // item categories
+}
