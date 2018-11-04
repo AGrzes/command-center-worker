6837192d512b9b4d751c218ff33d86c8fe0cf8df
Grzegorz Abramczyk <abramczykg@gmail.com>
2018-11-04 22:02:10 +0100

    Github issue fetch

--- src/github.ts
+++ src/github.ts
@@ -1,12 +1,21 @@
 import axios from 'axios'
+import * as debug from 'debug'
+import { json, Router } from 'express'
 import {parse as parseLinkHeader, Reference} from 'http-link-header'
-import {Observable, Observer} from 'rxjs'
+import { Ouch, override } from 'ouch-rx'
+import * as PouchDB from 'pouchdb-http'
+import {Observable, Observer, Subject} from 'rxjs'
+import { debounceTime, map, tap } from 'rxjs/operators'
+import { WorkerStatus } from './model'
+const log = debug('github:pump')
 interface FetchOptions {
   token: string
   since?: string
 }
 
-export function fetch(options: FetchOptions): Observable<any> {
+const token = process.env.GIT_HUB_TOKEN
+
+function fetch(options: FetchOptions): Observable<any> {
   return Observable.create((observer: Observer<any>) => {
     const doFetch = (address: string) => {
       axios.get(address, {
@@ -21,6 +30,8 @@ export function fetch(options: FetchOptions): Observable<any> {
         const next: Reference = links.rel('next')[0]
         if (next) {
           doFetch(next.uri)
+        } else {
+          observer.complete()
         }
       })
     }
@@ -33,5 +44,24 @@ export function fetch(options: FetchOptions): Observable<any> {
   })
 
 }
-let i = 0
-fetch({token: 'a47a8399ab3007b1b9aebe7b082f325d891a0257'}).subscribe((issue) => console.log(`${i++} : ${issue.title}`))
+const ouchGithub = new Ouch(new PouchDB('http://couchdb.home.agrzes.pl:5984/github'))
+const workerDb = new PouchDB<WorkerStatus>('http://couchdb.home.agrzes.pl:5984/worker')
+const ouchWorker = new Ouch(workerDb)
+const router = Router()
+router.post('/fetch', json(),  (req, res) => {
+  workerDb.get('github-progress-item-pump')
+  .catch((reason): PouchDB.Core.Document<WorkerStatus> => ({_id: 'github-progress-item-pump'}))
+  .then((workerStatus: PouchDB.Core.ExistingDocument<WorkerStatus>) => {
+    fetch({token , since: workerStatus.sequence as string})
+    .pipe(tap((issue) => {
+      if (issue.updated_at > workerStatus.sequence) {
+        workerStatus.sequence = issue.updated_at
+      }
+    }), map((issue) => ({...issue, _id: issue.id.toString()})), ouchGithub.merge(override))
+    .subscribe({complete() {
+      workerDb.put(workerStatus)
+      res.send()
+    }})
+  })
+})
+export default router
