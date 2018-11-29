44f825672413a12bd4647938a75e45be701a1866
Grzegorz Abramczyk <abramczykg@gmail.com>
2018-11-29 22:49:11 +0100

    Testing saved sequence

--- test/worker.ts
+++ test/worker.ts
@@ -3,29 +3,56 @@ import 'mocha'
 import { Ouch } from 'ouch-rx'
 import * as pouchdbAdapterMemory from 'pouchdb-adapter-memory'
 import * as PouchDB from 'pouchdb-core'
-import { empty } from 'rxjs'
+import { empty, of } from 'rxjs'
 import * as sinon from 'sinon'
 import * as sinonChai from 'sinon-chai'
 import {Worker} from '../src/worker'
+import { WorkerStatus } from '../src/model';
 PouchDB.plugin(pouchdbAdapterMemory)
 chai.use(sinonChai)
 const expect = chai.expect
-describe('Worker', function() {
+let dbIndex = 1
+function newDb<T>() {
+  return  new PouchDB<T>((dbIndex++).toString(), {adapter: 'memory'})
+}
 
+describe('Worker', function() {
+  const initialSequence = 'initialSequence'
+  const workerId = 'workerId'
+  const savedSequence = 'savedSequence'
   it('should use initial sequence if no one is saved', function(done) {
-    const workerDb = new PouchDB('1', {adapter: 'memory'})
-    const sink = new Ouch(new PouchDB('2', {adapter: 'memory'}))
+    const workerDb = newDb<WorkerStatus<string>>()
+    const sink = new Ouch(newDb())
     const source = sinon.mock().returns(empty())
     new Worker(workerDb,
-      'id',
+      workerId,
       source,
-      'initialSequence',
-      (item) => empty(),
+      initialSequence,
+      (item) => of<any>(),
       () => 'nextSequence',
       sink,
       (newDoc, oldDoc) => newDoc).run().subscribe({complete() {
-        expect(source).to.be.calledWith('initialSequence')
+        expect(source).to.be.calledWith(initialSequence)
         done()
       }})
+  })
+
+  it('should use saved sequence', function(done) {
+    const workerDb = newDb<WorkerStatus<string>>()
+    const sink = new Ouch(newDb())
+    const source = sinon.mock().returns(empty())
+    workerDb.put({_id: workerId, sequence: savedSequence}).then(() => {
+      new Worker(workerDb,
+        workerId,
+        source,
+        initialSequence,
+        (item) => of<any>(),
+        () => 'nextSequence',
+        sink,
+        (newDoc, oldDoc) => newDoc).run().subscribe({complete() {
+          expect(source).to.be.calledWith(savedSequence)
+          done()
+        }})
+    })
   })
 })
