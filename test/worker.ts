9cdb95b5f0c0af073ec44ed60a12bc557692feb9
Grzegorz Abramczyk <abramczykg@gmail.com>
2018-11-30 07:38:03 +0100

    Test for sequence save

--- test/worker.ts
+++ test/worker.ts
@@ -6,8 +6,8 @@ import * as PouchDB from 'pouchdb-core'
 import { empty, of } from 'rxjs'
 import * as sinon from 'sinon'
 import * as sinonChai from 'sinon-chai'
+import { WorkerStatus } from '../src/model'
 import {Worker} from '../src/worker'
-import { WorkerStatus } from '../src/model';
 PouchDB.plugin(pouchdbAdapterMemory)
 chai.use(sinonChai)
 const expect = chai.expect
@@ -20,16 +20,19 @@ describe('Worker', function() {
   const initialSequence = 'initialSequence'
   const workerId = 'workerId'
   const savedSequence = 'savedSequence'
+  const nextSequence = 'nextSequence'
+  const sourceItems = [{}]
+  const mappedItems = [{_id: '1'}]
   it('should use initial sequence if no one is saved', function(done) {
     const workerDb = newDb<WorkerStatus<string>>()
     const sink = new Ouch(newDb())
-    const source = sinon.mock().returns(empty())
+    const source = sinon.mock().returns(of(...sourceItems))
     new Worker(workerDb,
       workerId,
       source,
       initialSequence,
-      (item) => of<any>(),
-      () => 'nextSequence',
+      (item) => of<any>(mappedItems),
+      () => nextSequence,
       sink,
       (newDoc, oldDoc) => newDoc).run().subscribe({complete() {
         expect(source).to.be.calledWith(initialSequence)
@@ -40,19 +43,38 @@ describe('Worker', function() {
   it('should use saved sequence', function(done) {
     const workerDb = newDb<WorkerStatus<string>>()
     const sink = new Ouch(newDb())
-    const source = sinon.mock().returns(empty())
+    const source = sinon.mock().returns(of(...sourceItems))
     workerDb.put({_id: workerId, sequence: savedSequence}).then(() => {
       new Worker(workerDb,
         workerId,
         source,
         initialSequence,
-        (item) => of<any>(),
-        () => 'nextSequence',
+        (item) => of<any>(mappedItems),
+        () => nextSequence,
         sink,
         (newDoc, oldDoc) => newDoc).run().subscribe({complete() {
           expect(source).to.be.calledWith(savedSequence)
           done()
         }})
     })
+  })
+
+  it('should save sequence', function(done) {
+    const workerDb = newDb<WorkerStatus<string>>()
+    const sink = new Ouch(newDb())
+    const source = sinon.mock().returns(of(...sourceItems))
+    new Worker(workerDb,
+      workerId,
+      source,
+      initialSequence,
+      (item) => of<any>(mappedItems),
+      () => nextSequence,
+      sink,
+      (newDoc, oldDoc) => newDoc).run().subscribe({complete() {
+        workerDb.get(workerId).then((status) => {
+          expect(status).to.have.property('sequence', nextSequence)
+          done()
+        }).catch(done)
+      }})
   })
 })
