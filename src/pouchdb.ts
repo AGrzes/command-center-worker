9efeb4bfba4a0071ad0e266bdb7ae74d421f4cdf
Grzegorz Abramczyk <abramczykg@gmail.com>
2018-12-09 17:17:35 +0100

    Moved configureDb function to pouchdb module

--- src/pouchdb.ts
+++ src/pouchdb.ts
@@ -1,6 +1,34 @@
+import {Agent} from 'https'
+import fetch, { Request } from 'node-fetch'
 import * as pouchdbAdapterHttp from 'pouchdb-adapter-http'
 import * as PouchDB from 'pouchdb-core'
 import * as pouchdbMapReduce from 'pouchdb-mapreduce'
 import * as pouchdbReplication from 'pouchdb-replication'
 PouchDB.plugin(pouchdbReplication).plugin(pouchdbMapReduce).plugin(pouchdbAdapterHttp)
 export default PouchDB
+
+export interface ServerConfig {
+  url: string,
+  key?: string
+  cert?: string
+}
+
+export interface DbConfig {
+  server: ServerConfig
+  name: string
+}
+export function configureDb<X>(config: DbConfig): PouchDB.Database<X> {
+  const dbOptions: PouchDB.Configuration.RemoteDatabaseConfiguration = {}
+  if (config.server.key || config.server.cert) {
+    const agent = new Agent({
+      key: config.server.key,
+      cert: config.server.cert
+    })
+    dbOptions.fetch = (url: string | Request, options) => {
+      return fetch(url, { ...options,
+        agent
+      })
+    }
+  }
+  return new PouchDB(`${config.server.url}/${config.name}`, dbOptions)
+}
--- src/sync.ts
+++ src/sync.ts
@@ -1,42 +1,14 @@
 import * as debug from 'debug'
 import {readFile} from 'fs'
-import {Agent} from 'https'
 import * as yaml from 'js-yaml'
 import * as _ from 'lodash'
-import fetch, { Request } from 'node-fetch'
-import PouchDB from './pouchdb'
+import { configureDb, DbConfig } from './pouchdb'
 import sync from './service/sync'
 const log = debug('sync')
-interface ServerConfig {
-  url: string,
-  key?: string
-  cert?: string
-}
-
-interface DbConfig {
-  server: ServerConfig
-  name: string
-}
 
 interface SyncConfig {
   from: DbConfig
   to: DbConfig
-}
-
-function configureDb<X>(config: DbConfig): PouchDB.Database<X> {
-  const dbOptions: PouchDB.Configuration.RemoteDatabaseConfiguration = {}
-  if (config.server.key || config.server.cert) {
-    const agent = new Agent({
-      key: config.server.key,
-      cert: config.server.cert
-    })
-    dbOptions.fetch = (url: string | Request, options) => {
-      return fetch(url, { ...options,
-        agent
-      })
-    }
-  }
-  return new PouchDB(`${config.server.url}/${config.name}`, dbOptions)
 }
 
 readFile('config/sync.yaml', 'UTF-8', (err, data) => {
