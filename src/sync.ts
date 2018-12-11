87030dd9a7c67ded74ac8679d2b3c1fbcafcf074
Grzegorz Abramczyk <abramczykg@gmail.com>
2018-12-11 07:16:36 +0100

    Moved loading config to separate file

--- src/sync.ts
+++ src/sync.ts
@@ -1,18 +1,10 @@
 import * as debug from 'debug'
-import {readFile} from 'fs'
-import * as yaml from 'js-yaml'
-import * as _ from 'lodash'
+import loadConfig from './config/sync'
 import setUpSync from './controller/sync'
-import { SyncConfig } from './model/sync'
 import { configureDb } from './pouchdb'
 import sync from './service/sync'
 const log = debug('sync')
 
-readFile('config/sync.yaml', 'UTF-8', (err, data) => {
-  if (err) {
-    log(err)
-  } else {
-    const config: SyncConfig[] = yaml.load(data)
-    setUpSync(config, sync, configureDb)
-  }
-})
+loadConfig('config/sync.yaml')
+  .then((config) => setUpSync(config, sync, configureDb))
+  .catch(log)
