701ec2039149d5c138387f5c3a781f7c9bc5306f
Grzegorz Abramczyk <abramczykg@gmail.com>
2018-12-13 21:38:46 +0100

    Basic test for issueToExerciseSession

--- src/worker/exercise-session-worker.ts
+++ src/worker/exercise-session-worker.ts
@@ -1,6 +1,6 @@
 import { Ouch } from 'ouch-rx'
 import { empty, Observable } from 'rxjs'
-import { WorkerStatus } from '../model'
+import { ProgressItem, WorkerStatus } from '../model'
 import { Worker } from '../worker'
 type Activity = 'run' | 'pool' | 'crunches' | 'bike'
 type Unit = 'session' | 'm' | 'km'
@@ -11,16 +11,17 @@ export interface ExerciseSession {
   unit?: Unit
 }
 
-export function issueToExerciseSession(change: any): Observable<PouchDB.Core.Document<ExerciseSession>> {
-  return empty()
+export function issueToExerciseSession(patterns: RegExp[]):
+  (change: ProgressItem) => Observable<PouchDB.Core.Document<ExerciseSession>> {
+  return (change: ProgressItem) => empty()
 }
 
 export default function worker(workerDb: PouchDB.Database<WorkerStatus<string>>,
-                               ouchJira: Ouch<any>,
+                               sourceOuch: Ouch<any>,
                                ouchExerciseSession: Ouch<any>,
                                mapFunction: (item: any) => Observable<ExerciseSession & PouchDB.Core.IdMeta>):
                                Worker<any, string, ExerciseSession> {
   return new Worker(workerDb, 'exercise-session',
-  (sequence) => ouchJira.changes<any>({include_docs: true, live: true, since: sequence }), '',
+  (sequence) => sourceOuch.changes<any>({include_docs: true, live: true, since: sequence }), '',
   mapFunction, (change) => change.seq, ouchExerciseSession)
 }
--- test/worker/exercise-session-worker.ts
+++ test/worker/exercise-session-worker.ts
@@ -5,6 +5,7 @@ import * as sinon from 'sinon'
 import * as sinonChai from 'sinon-chai'
 import * as worker from '../../src/worker'
 import * as exerciseSessionWorker from '../../src/worker/exercise-session-worker'
+import { toArray } from 'rxjs/operators';
 chai.use(sinonChai)
 const expect = chai.expect
 describe('worker', function() {
@@ -13,7 +14,7 @@ describe('worker', function() {
       const workerConstructorSpy = sinon.spy(worker, 'Worker')
       const workerDb: any = {}
       const changesMock = sinon.mock()
-      const ouchJira: any = {changes: changesMock}
+      const sourceOuch: any = {changes: changesMock}
       const ouchExerciseSession: any = {}
       const seq = 'seq'
       const data = {}
@@ -26,27 +27,38 @@ describe('worker', function() {
         workerConstructorSpy.restore()
       })
       it('should create worker', function() {
-        const instance = exerciseSessionWorker.default(workerDb, ouchJira, ouchExerciseSession, map)
+        const instance = exerciseSessionWorker.default(workerDb, sourceOuch, ouchExerciseSession, map)
         expect(instance).to.be.instanceOf(worker.Worker)
       })
       it('should initialize worker', function() {
-        exerciseSessionWorker.default(workerDb, ouchJira, ouchExerciseSession, map)
+        exerciseSessionWorker.default(workerDb, sourceOuch, ouchExerciseSession, map)
         expect(workerConstructorSpy).to.be.calledOnceWith(workerDb, 'exercise-session', sinon.match.func, '',
           map, sinon.match.func, ouchExerciseSession)
       })
 
       it('should use `change.seq` to measure progress', function() {
-        exerciseSessionWorker.default(workerDb, ouchJira, ouchExerciseSession, map)
+        exerciseSessionWorker.default(workerDb, sourceOuch, ouchExerciseSession, map)
         const sequenceFunction: (item: {seq: string}) => string = workerConstructorSpy.firstCall.args[5]
         expect(sequenceFunction({seq})).to.be.equal(seq)
       })
 
       it('should use `changes` from `ouchJira`', function() {
-        exerciseSessionWorker.default(workerDb, ouchJira, ouchExerciseSession, map)
+        exerciseSessionWorker.default(workerDb, sourceOuch, ouchExerciseSession, map)
         const sourceFunction: (sequence: string) => Observable<any> = workerConstructorSpy.firstCall.args[2]
         changesMock.returns(data)
         expect(sourceFunction(seq)).to.be.equal(data)
         expect(changesMock).to.be.calledOnceWith({include_docs: true, live: true, since: seq })
+      })
+    })
+    describe('issueToExerciseSession', function() {
+      it('should return empty observable when summary does not match any of patterns', function(done) {
+        exerciseSessionWorker.issueToExerciseSession([])({
+          summary: 'Test',
+          status: 'resolved'
+        }).pipe(toArray()).subscribe((result) => {
+          expect(result).to.be.empty
+          done()
+        }, done)
       })
     })
   })
