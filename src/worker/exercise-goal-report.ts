42480cc0c03a5ffafd90aba290b8e9b15a073167
Grzegorz Abramczyk <abramczykg@gmail.com>
2018-12-16 20:35:19 +0100

    Calculate progress function with basic tests

---
+++ src/worker/exercise-goal-report.ts
@@ -0,0 +1,102 @@
+import * as _ from 'lodash'
+import { Ouch, override } from 'ouch-rx'
+import { from, Observable } from 'rxjs'
+import { filter, flatMap } from 'rxjs/operators'
+import { Activity, ExerciseSession, Unit } from './exercise-session-worker'
+
+export interface Goal {
+  activity: Activity
+  startDate: string
+  dueDate: string
+  target: number
+  unit?: Unit
+  archived: boolean
+  meet?: boolean
+}
+
+interface ProgressItem {
+  date: string
+  increment: number
+  total: number
+}
+
+interface GoalReport extends Goal {
+  progress: ProgressItem[]
+}
+
+export function normalizeValue(value: number, valueUnit: Unit, targetUnit) {
+  switch (targetUnit) {
+    case 'm':
+      switch (valueUnit) {
+        case 'm': return value
+        case 'km': return value * 1000
+        default: return null
+      }
+    case 'km':
+      switch (valueUnit) {
+        case 'm': return value / 1000
+        case 'km': return value
+        default: return null
+      }
+    case 'session': return 1
+  }
+}
+
+export function calculateProgress(goal: PouchDB.Core.Document<Goal>,
+                                  response: PouchDB.Query.Response<ExerciseSession>):
+  PouchDB.Core.Document<GoalReport> {
+  const progress: ProgressItem[] = []
+  let lastDate = null
+  let progressItem: ProgressItem = null
+  let total = 0
+  for (const row of response.rows) {
+    const date = row.doc.date.substring(0, 10)
+    const increment = normalizeValue(row.doc.progress, row.doc.unit, goal.unit)
+    if (increment) {
+      if (date !== lastDate) {
+        lastDate = date
+        total += increment
+        progressItem = {
+          date,
+          increment,
+          total
+        }
+        progress.push(progressItem)
+      } else {
+        total += increment
+        progressItem.total = total
+        progressItem.increment += increment
+      }
+    }
+  }
+  return Object.assign({ progress}, goal, {_rev: undefined})
+}
+
+export function generateGoalReport(goal: PouchDB.Core.Document<Goal>, sessionDb: PouchDB.Database<ExerciseSession> ):
+  Observable<PouchDB.Core.Document<GoalReport>> {
+  return from(
+  sessionDb.query<ExerciseSession>('index/activity-date', {
+    include_docs: true,
+    startkey: [
+      goal.activity,
+      goal.startDate.substring(0, 4),
+      goal.startDate.substring(5, 7),
+      goal.startDate.substring(8, 10)
+    ],
+    endkey: [
+      goal.activity,
+      goal.dueDate.substring(0, 4),
+      goal.dueDate.substring(5, 7),
+      goal.dueDate.substring(8, 10)
+    ],
+    inclusive_end: true
+  }).then((response) => calculateProgress(goal, response)))
+}
+
+export function generateGoalReports(goalOuch: Ouch<Goal>,
+                                    goalReportOuch: Ouch<GoalReport>,
+                                    sessionDb: PouchDB.Database<ExerciseSession> ): Observable<any> {
+  return goalOuch.all().pipe(filter((goal) => !goal._id.startsWith('_')), flatMap((goal) => {
+    return generateGoalReport(goal, sessionDb)
+  }), goalReportOuch.merge(override))
+}
---
+++ test/worker/exercise-goal.report.ts
@@ -0,0 +1,84 @@
+import * as chai from 'chai'
+import chaiSubset = require('chai-subset')
+import 'mocha'
+import { toArray } from 'rxjs/operators'
+import * as sinonChai from 'sinon-chai'
+import { ProgressItem } from '../../src/model'
+import * as exerciseGoalReport from '../../src/worker/exercise-goal-report'
+import { ExerciseSession } from '../../src/worker/exercise-session-worker'
+chai.use(sinonChai).use(chaiSubset)
+const expect = chai.expect
+describe('worker', function() {
+  describe('exercise-goal-report', function() {
+    describe('issueToExerciseSession', function() {
+      const response: PouchDB.Query.Response<ExerciseSession> = {
+        offset: 0,
+        total_rows: 3,
+        rows: [{
+          id: 'id',
+          key: [],
+          doc: {
+            _id: 'id',
+            _rev: null,
+            date: '2000-01-01',
+            activity: 'run',
+            progress: 1000,
+            unit: 'm'
+          },
+          value: null
+        }, {
+          id: 'id',
+          key: [],
+          doc: {
+            _id: 'id',
+            _rev: null,
+            date: '2000-01-01',
+            activity: 'run',
+            progress: 1,
+            unit: 'km'
+          },
+          value: null
+        }, {
+          id: 'id',
+          key: [],
+          doc: {
+            _id: 'id',
+            _rev: null,
+            date: '2000-01-02',
+            activity: 'run',
+            progress: 1000,
+            unit: 'm'
+          },
+          value: null
+        }]
+      }
+      const goal: PouchDB.Core.ExistingDocument<exerciseGoalReport.Goal> = {
+        activity: 'run',
+        archived: false,
+        startDate: '2000-01-01',
+        dueDate: '2001-01-01',
+        target: 100,
+        unit: 'km',
+        meet: true,
+        _id: '_id',
+        _rev: '_rev'
+      }
+      it('should copy goal attributes except `_rev`', function() {
+        expect(exerciseGoalReport.calculateProgress(goal, response))
+          .to.containSubset(Object.assign({}, goal, {_rev: undefined}))
+      })
+      it('should calculate increments and total', function() {
+        expect(exerciseGoalReport.calculateProgress(goal, response))
+          .to.containSubset({progress: [{
+            date: '2000-01-01',
+            increment: 2,
+            total: 2
+          }, {
+            date: '2000-01-02',
+            increment: 1,
+            total: 3
+          }]})
+      })
+    })
+  })
+})
